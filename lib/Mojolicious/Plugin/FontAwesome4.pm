3c7666c25ef721de3b961a379bcfd5bec8463a41
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-28 22:08:53 +0200

    Released version 4.2000
           * Updated to Font-Awesome v4.2.0
           * Fix unit test

--- lib/Mojolicious/Plugin/FontAwesome4.pm
+++ lib/Mojolicious/Plugin/FontAwesome4.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::FontAwesome4 - Mojolicious + http://fortawesome.github.io/F
 
 =head1 VERSION
 
-4.1000
+4.2000
 
 =head1 DESCRIPTION
 
@@ -42,7 +42,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use File::Spec::Functions 'catdir';
 use Cwd ();
 
-our $VERSION = '4.1000';
+our $VERSION = '4.2000';
 
 =head1 HELPERS
 
